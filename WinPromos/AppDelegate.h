6d4faf4f35439b3304fd3df615b177d007270479
Olivier <olivier.d.sans@gmail.com>
2015-03-16 12:08:16 +0100

    Initial Commit

---
+++ WinPromos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WinPromos
+//
+//  Created by olivier on 2015-03-16.
+//  Copyright (c) 2015 IronHack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
